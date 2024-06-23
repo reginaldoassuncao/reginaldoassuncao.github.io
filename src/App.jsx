f87093db993306adb98d8f3b066b97ac4d777b6b
Reginaldo Assunção <regi.assuncao@hotmail.com>
2024-06-23 12:23:28 -0300

    Criado o Footer.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@
 import React from 'react';
 import Header from './components/Header';
 import ProjectList from './components/ProjectList';
+import Footer from './components/Footer';
 
 function App() {
   return (
     <div>
       <Header />
       <ProjectList />
+      <Footer />
     </div>
   );
 }
---
+++ src/components/Footer.jsx
@@ -0,0 +1,12 @@
+// src/components/Footer.jsx
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <footer className="footer">
+      <p>Desenvolvido por Reginaldo Assunção - 2024</p>
+    </footer>
+  );
+}
+
+export default Footer;
